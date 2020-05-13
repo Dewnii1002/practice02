fe59510676c6253b3160292d22c6adc45c2d6d94
bit <bit@DESKTOP-2KUPKOU>
2020-05-13 21:52:30 +0900

    20200513_Practice

--- src/practice02/Prob01.java
+++ src/practice02/Prob01.java
@@ -6,14 +6,29 @@ public class Prob01 {
 	public static void main(String[] args) {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
+		System.out.print("금액: ");
 		Scanner scanner = new Scanner(System.in);
+		String moneys = scanner.nextLine();
 
+		int length = moneys.length();
 
-		/* 코드를 완성합니다 */
-
+		int money = Integer.parseInt(moneys);
+		int[] temp = new int[length];
+		int[] change = new int[length];
 		
-		
+		change[0] = money;
+
+		for (int i = 0; i < length; i++) {
+			if (change[i] / MONEYS[i] >= 1) {
+				temp[i] = change[i] / MONEYS[i];
+				change[i+1] = change[i] - MONEYS[i]*temp[i];
+				System.out.println(temp[i]);
+			}
+
+		}
 		scanner.close();
 	}
+
+	/* 코드를 완성합니다 */
 
 }
--- src/practice02/Prob05.java
+++ src/practice02/Prob05.java
@@ -1,12 +1,26 @@
 package practice02;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Prob05 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
+		Random r = new Random();
+		int k = r.nextInt(100) +1;
+		int tryCount = 1;
 		
+		while(true) {
+			System.out.print(tryCount+">>");
+			int input = scanner.nextInt();
+			
+			if(k==input) {
+				break;
+			}
+			
+			tryCount++;
+		}
 
 		
 		/* 코드를 완성합니다 */
